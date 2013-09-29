162bcc94fc913492a08f9f24d6457669376675a5
Brandon Barker <bbark@verizon.net>
2013-09-29 14:12:49 -0400

    SimplePatch

--- src/main/java/com/projectbarks/nxannouncer/announcer/Timer.java
+++ src/main/java/com/projectbarks/nxannouncer/announcer/Timer.java
@@ -19,25 +19,27 @@ public class Timer extends BukkitRunnable implements Runnable {
 
     @Override
     public void run() {
-        if (main.getServer().getOnlinePlayers().length <= 0 && config.isNoPlayers()) {
-            NXAnnouncer.getLOG().log(Level.INFO, "Not enough players broadcast cancled");
-            return;
+        if (config.getAnnouncements().size() > 0) {
+            if (main.getServer().getOnlinePlayers().length <= 0 && config.isNoPlayers()) {
+                NXAnnouncer.getLOG().log(Level.INFO, "Not enough players broadcast cancled");
+                return;
+            }
+            Announcement announcement = config.getAnnouncements().get(getCount());
+            this.broadcast(config.getColor() + announcement.getColorizedHeader());
+            for (String message : announcement.getTranslatedMessage()) {
+                String colorizedMessage = Announcement.colorize(message);
+                String dynamicChar = Announcement.formatChar(colorizedMessage,
+                                                             announcement.getColorizedHeader(),
+                                                             announcement.getColorizedFooter());
+                this.broadcast(config.getColor() + dynamicChar + colorizedMessage);
+            }
+            this.broadcast(config.getColor() + announcement.getColorizedFooter());
+            setCount(count + 1);
+            if (getCount() >= config.getAnnouncements().size()) {
+                setCount(0);
+            }
+            NXAnnouncer.getLOG().log(Level.INFO, "{0}Message {1} has been sent to {2} players.", new Object[]{config.getPluginName(), getCount(), main.getServer().getOnlinePlayers().length});
         }
-        Announcement announcement = config.getAnnouncements().get(getCount());
-        this.broadcast(config.getColor() + announcement.getColorizedHeader());
-        for (String message : announcement.getTranslatedMessage()) {
-            String colorizedMessage = Announcement.colorize(message);
-            String dynamicChar = Announcement.formatChar(colorizedMessage,
-                                                         announcement.getColorizedHeader(),
-                                                         announcement.getColorizedFooter());
-            this.broadcast(config.getColor() + dynamicChar + colorizedMessage);
-        }
-        this.broadcast(config.getColor() + announcement.getColorizedFooter());
-        setCount(count + 1);
-        if (getCount() >= config.getAnnouncements().size()) {
-            setCount(0);
-        }
-        NXAnnouncer.getLOG().log(Level.INFO, "{0}Message {1} has been sent to {2} players.", new Object[]{config.getPluginName(), getCount(), main.getServer().getOnlinePlayers().length});
     }
 
     private void broadcast(String message) {
